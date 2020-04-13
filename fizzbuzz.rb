08aaeafd07102d94fec7629a2e576cbb7bec1a40
Yuliya Yurkevich <juliana.ny2008@gmail.com>
2020-04-13 17:08:21 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,8 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def fizzbuzz
-  
+def fizzbuzz(int)
+ if int % 3 == 0 
+   puts "Fizz"
+end
 end
