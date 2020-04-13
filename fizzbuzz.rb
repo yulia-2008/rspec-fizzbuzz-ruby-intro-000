acdf83789fd37716efe87a6ea0ce74e6133abb34
Yuliya Yurkevich <juliana.ny2008@gmail.com>
2020-04-13 17:57:24 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,7 +5,7 @@ def fizzbuzz(int)
  if int % 3 == 0 && int % 5 != 0
   fizz_3 = "Fizz"
 elsif
-  int % 5 == 0
+  int % 5 == 0 && int % 3 != 0
   fizz_5 = "Buzz"
 elsif
   int % 3 == 0 && int % 5 == 0
